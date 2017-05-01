e316303bbe840f6adf57f6d42ec8c42c1d76afdf
Adam Michaud <invariel@gmail.com>
2017-05-01 19:10:14 -0400

    Initial commit.

---
+++ Isaac Game Tracker/Isaac Game Tracker/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace IGGnis_Game_Tracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmb_Character = new System.Windows.Forms.ComboBox();
+            this.btn_Victory = new System.Windows.Forms.Button();
+            this.btn_Defeat = new System.Windows.Forms.Button();
+            this.dgv_Record = new System.Windows.Forms.DataGridView();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.lbl_Wins = new System.Windows.Forms.Label();
+            this.lbl_WinCount = new System.Windows.Forms.Label();
+            this.lbl_Losses = new System.Windows.Forms.Label();
+            this.lbl_LossCount = new System.Windows.Forms.Label();
+            this.lbl_MostWins = new System.Windows.Forms.Label();
+            this.lbl_MostWinsText = new System.Windows.Forms.Label();
+            this.lbl_FewestWins = new System.Windows.Forms.Label();
+            this.lbl_FewestWinsText = new System.Windows.Forms.Label();
+            this.lbl_LongestStreak = new System.Windows.Forms.Label();
+            this.lbl_LongestStreakNumber = new System.Windows.Forms.Label();
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.lbl_CurrentStreak = new System.Windows.Forms.Label();
+            this.lbl_CurrentStreakNumber = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Record)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmb_Character
+            // 
+            this.cmb_Character.FormattingEnabled = true;
+            this.cmb_Character.Location = new System.Drawing.Point(13, 13);
+            this.cmb_Character.Name = "cmb_Character";
+            this.cmb_Character.Size = new System.Drawing.Size(151, 21);
+            this.cmb_Character.TabIndex = 0;
+            this.cmb_Character.Text = "Eden";
+            // 
+            // btn_Victory
+            // 
+            this.btn_Victory.Location = new System.Drawing.Point(171, 13);
+            this.btn_Victory.Name = "btn_Victory";
+            this.btn_Victory.Size = new System.Drawing.Size(75, 23);
+            this.btn_Victory.TabIndex = 1;
+            this.btn_Victory.Text = "VICTORY!";
+            this.btn_Victory.UseVisualStyleBackColor = true;
+            this.btn_Victory.Click += new System.EventHandler(this.btn_Victory_Click);
+            // 
+            // btn_Defeat
+            // 
+            this.btn_Defeat.Location = new System.Drawing.Point(253, 13);
+            this.btn_Defeat.Name = "btn_Defeat";
+            this.btn_Defeat.Size = new System.Drawing.Size(75, 23);
+            this.btn_Defeat.TabIndex = 2;
+            this.btn_Defeat.Text = "Defeat";
+            this.btn_Defeat.UseVisualStyleBackColor = true;
+            this.btn_Defeat.Click += new System.EventHandler(this.btn_Defeat_Click);
+            // 
+            // dgv_Record
+            // 
+            this.dgv_Record.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Record.Location = new System.Drawing.Point(13, 41);
+            this.dgv_Record.Name = "dgv_Record";
+            this.dgv_Record.Size = new System.Drawing.Size(315, 179);
+            this.dgv_Record.TabIndex = 3;
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(334, 13);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(85, 23);
+            this.btn_Clear.TabIndex = 4;
+            this.btn_Clear.Text = "Clear All";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // lbl_Wins
+            // 
+            this.lbl_Wins.AutoSize = true;
+            this.lbl_Wins.Location = new System.Drawing.Point(347, 43);
+            this.lbl_Wins.Name = "lbl_Wins";
+            this.lbl_Wins.Size = new System.Drawing.Size(31, 13);
+            this.lbl_Wins.TabIndex = 5;
+            this.lbl_Wins.Text = "Wins";
+            // 
+            // lbl_WinCount
+            // 
+            this.lbl_WinCount.AutoSize = true;
+            this.lbl_WinCount.Location = new System.Drawing.Point(384, 43);
+            this.lbl_WinCount.Name = "lbl_WinCount";
+            this.lbl_WinCount.Size = new System.Drawing.Size(35, 13);
+            this.lbl_WinCount.TabIndex = 6;
+            this.lbl_WinCount.Text = "label1";
+            // 
+            // lbl_Losses
+            // 
+            this.lbl_Losses.AutoSize = true;
+            this.lbl_Losses.Location = new System.Drawing.Point(338, 60);
+            this.lbl_Losses.Name = "lbl_Losses";
+            this.lbl_Losses.Size = new System.Drawing.Size(40, 13);
+            this.lbl_Losses.TabIndex = 7;
+            this.lbl_Losses.Text = "Losses";
+            // 
+            // lbl_LossCount
+            // 
+            this.lbl_LossCount.AutoSize = true;
+            this.lbl_LossCount.Location = new System.Drawing.Point(384, 60);
+            this.lbl_LossCount.Name = "lbl_LossCount";
+            this.lbl_LossCount.Size = new System.Drawing.Size(35, 13);
+            this.lbl_LossCount.TabIndex = 8;
+            this.lbl_LossCount.Text = "label1";
+            // 
+            // lbl_MostWins
+            // 
+            this.lbl_MostWins.AutoSize = true;
+            this.lbl_MostWins.Location = new System.Drawing.Point(331, 85);
+            this.lbl_MostWins.Name = "lbl_MostWins";
+            this.lbl_MostWins.Size = new System.Drawing.Size(82, 13);
+            this.lbl_MostWins.TabIndex = 9;
+            this.lbl_MostWins.Text = "Most Wins With";
+            // 
+            // lbl_MostWinsText
+            // 
+            this.lbl_MostWinsText.AutoSize = true;
+            this.lbl_MostWinsText.Location = new System.Drawing.Point(384, 100);
+            this.lbl_MostWinsText.Name = "lbl_MostWinsText";
+            this.lbl_MostWinsText.Size = new System.Drawing.Size(35, 13);
+            this.lbl_MostWinsText.TabIndex = 10;
+            this.lbl_MostWinsText.Text = "label1";
+            this.lbl_MostWinsText.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lbl_FewestWins
+            // 
+            this.lbl_FewestWins.AutoSize = true;
+            this.lbl_FewestWins.Location = new System.Drawing.Point(331, 117);
+            this.lbl_FewestWins.Name = "lbl_FewestWins";
+            this.lbl_FewestWins.Size = new System.Drawing.Size(93, 13);
+            this.lbl_FewestWins.TabIndex = 11;
+            this.lbl_FewestWins.Text = "Fewest Wins With";
+            // 
+            // lbl_FewestWinsText
+            // 
+            this.lbl_FewestWinsText.AutoSize = true;
+            this.lbl_FewestWinsText.Location = new System.Drawing.Point(384, 133);
+            this.lbl_FewestWinsText.Name = "lbl_FewestWinsText";
+            this.lbl_FewestWinsText.Size = new System.Drawing.Size(35, 13);
+            this.lbl_FewestWinsText.TabIndex = 12;
+            this.lbl_FewestWinsText.Text = "label1";
+            this.lbl_FewestWinsText.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lbl_LongestStreak
+            // 
+            this.lbl_LongestStreak.AutoSize = true;
+            this.lbl_LongestStreak.Location = new System.Drawing.Point(331, 148);
+            this.lbl_LongestStreak.Name = "lbl_LongestStreak";
+            this.lbl_LongestStreak.Size = new System.Drawing.Size(79, 13);
+            this.lbl_LongestStreak.TabIndex = 13;
+            this.lbl_LongestStreak.Text = "Longest Streak";
+            // 
+            // lbl_LongestStreakNumber
+            // 
+            this.lbl_LongestStreakNumber.AutoSize = true;
+            this.lbl_LongestStreakNumber.Location = new System.Drawing.Point(384, 163);
+            this.lbl_LongestStreakNumber.Name = "lbl_LongestStreakNumber";
+            this.lbl_LongestStreakNumber.Size = new System.Drawing.Size(35, 13);
+            this.lbl_LongestStreakNumber.TabIndex = 14;
+            this.lbl_LongestStreakNumber.Text = "label1";
+            this.lbl_LongestStreakNumber.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // form1BindingSource
+            // 
+            this.form1BindingSource.DataSource = typeof(IGGnis_Game_Tracker.Form1);
+            // 
+            // lbl_CurrentStreak
+            // 
+            this.lbl_CurrentStreak.AutoSize = true;
+            this.lbl_CurrentStreak.Location = new System.Drawing.Point(334, 184);
+            this.lbl_CurrentStreak.Name = "lbl_CurrentStreak";
+            this.lbl_CurrentStreak.Size = new System.Drawing.Size(75, 13);
+            this.lbl_CurrentStreak.TabIndex = 15;
+            this.lbl_CurrentStreak.Text = "Current Streak";
+            // 
+            // lbl_CurrentStreakNumber
+            // 
+            this.lbl_CurrentStreakNumber.AutoSize = true;
+            this.lbl_CurrentStreakNumber.Location = new System.Drawing.Point(384, 202);
+            this.lbl_CurrentStreakNumber.Name = "lbl_CurrentStreakNumber";
+            this.lbl_CurrentStreakNumber.Size = new System.Drawing.Size(35, 13);
+            this.lbl_CurrentStreakNumber.TabIndex = 16;
+            this.lbl_CurrentStreakNumber.Text = "label1";
+            this.lbl_CurrentStreakNumber.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 265);
+            this.Controls.Add(this.lbl_CurrentStreakNumber);
+            this.Controls.Add(this.lbl_CurrentStreak);
+            this.Controls.Add(this.lbl_LongestStreakNumber);
+            this.Controls.Add(this.lbl_LongestStreak);
+            this.Controls.Add(this.lbl_FewestWinsText);
+            this.Controls.Add(this.lbl_FewestWins);
+            this.Controls.Add(this.lbl_MostWinsText);
+            this.Controls.Add(this.lbl_MostWins);
+            this.Controls.Add(this.lbl_LossCount);
+            this.Controls.Add(this.lbl_Losses);
+            this.Controls.Add(this.lbl_WinCount);
+            this.Controls.Add(this.lbl_Wins);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.dgv_Record);
+            this.Controls.Add(this.btn_Defeat);
+            this.Controls.Add(this.btn_Victory);
+            this.Controls.Add(this.cmb_Character);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Record)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmb_Character;
+        private System.Windows.Forms.Button btn_Victory;
+        private System.Windows.Forms.Button btn_Defeat;
+        private System.Windows.Forms.DataGridView dgv_Record;
+        private System.Windows.Forms.BindingSource form1BindingSource;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.Label lbl_Wins;
+        private System.Windows.Forms.Label lbl_WinCount;
+        private System.Windows.Forms.Label lbl_Losses;
+        private System.Windows.Forms.Label lbl_LossCount;
+        private System.Windows.Forms.Label lbl_MostWins;
+        private System.Windows.Forms.Label lbl_MostWinsText;
+        private System.Windows.Forms.Label lbl_FewestWins;
+        private System.Windows.Forms.Label lbl_FewestWinsText;
+        private System.Windows.Forms.Label lbl_LongestStreak;
+        private System.Windows.Forms.Label lbl_LongestStreakNumber;
+        private System.Windows.Forms.Label lbl_CurrentStreak;
+        private System.Windows.Forms.Label lbl_CurrentStreakNumber;
+    }
+}
+
---
+++ Isaac Game Tracker/Isaac Game Tracker/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IGGnis_Game_Tracker
+{
+    public partial class Form1 : Form
+    {
+        BindingList<Game> games = new BindingList<Game> ();
+        static String[] characters = { "Isaac", "Magdalene", "Cain", "Judas", "???", "Eve", "Samson", "Azazel", "Lazarus", "Eden", "The Lost", "Lilith", "Keeper", "Apollyon" };
+
+        int[] wins = new int[characters.Length];
+        int[] streaks = new int[characters.Length + 1];
+        int[] longeststreaks = new int[characters.Length + 1];
+
+        int victories = 0;
+        int defeats = 0;
+        int longeststreak = 0;
+        int currentstreak = 0;
+
+        int numberofgames = 10000;
+
+        public class Game
+        {
+            public String Character { get; set; }
+            public Boolean WonRun { get; set; }
+
+            public Game(String character, Boolean victory)
+            {
+                this.Character = character;
+                this.WonRun = victory;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            victories = 0;
+            defeats = 0;
+            currentstreak = 0;
+            longeststreak = 0;
+
+            LoadGames();
+            UpdateLabels();
+            CalculateStats();
+
+            cmb_Character.DataSource = characters;
+            dgv_Record.DataSource = games;
+        }
+
+        private void btn_Victory_Click(object sender, EventArgs e)
+        {
+            Game thisGame = new IGGnis_Game_Tracker.Form1.Game(cmb_Character.SelectedValue.ToString(), true);
+            AddGame(thisGame, true);
+        }
+
+        private void btn_Defeat_Click(object sender, EventArgs e)
+        {
+            Game thisGame = new IGGnis_Game_Tracker.Form1.Game(cmb_Character.SelectedValue.ToString(), false);
+            AddGame(thisGame, true);
+        }
+
+        private void CalculateStats ()
+        {
+
+            for (int x = 0; x < characters.Length; ++ x)
+            {
+                wins[x] = 0;
+                streaks[x] = 0;
+                longeststreaks[x] = 0;
+            }
+
+            streaks[characters.Length] = 0;
+            longeststreaks[characters.Length] = 0;
+
+            for (int g = games.Count - 1; g >= 0; --g)
+            {
+                string characterName = games[g].Character;
+                int characterNumber = Array.IndexOf(characters, characterName);
+
+                if (games[g].WonRun)
+                {
+                    streaks[characterNumber] ++;
+                    streaks[characters.Length] ++;
+                    wins[characterNumber]++;
+                }
+                else
+                {
+                    if (streaks[characters.Length] > longeststreaks[characters.Length])
+                    {
+                        longeststreaks[characters.Length] = streaks[characters.Length];
+                        streaks[characters.Length] = 0;
+                    }
+                    if (streaks [characterNumber] > longeststreaks[characterNumber])
+                    {
+                        longeststreaks[characterNumber] = streaks[characterNumber];
+                        streaks[characterNumber] = 0;
+                    }
+                }
+            }
+
+            for (int c = 0; c <= characters.Length; ++ c)
+            {
+                if (streaks [c] > longeststreaks [c])
+                {
+                    longeststreaks[c] = streaks[c];
+                }
+            }
+
+            int max = wins.Max();
+            int min = wins.Min();
+
+            if (min == 0 && max != 0)
+            {
+                min = wins.OrderByDescending(x => x).Distinct().Reverse().Skip(1).First();
+            }
+
+            lbl_MostWinsText.Text = String.Format("{0} - {1}", characters[Array.IndexOf(wins, max)], max);
+            lbl_FewestWinsText.Text = String.Format("{0} - {1}", characters[Array.IndexOf(wins, min)], min);
+            lbl_LongestStreakNumber.Text = longeststreak.ToString();
+            lbl_CurrentStreakNumber.Text = currentstreak.ToString();
+        }
+
+        private void AddGame (Game g, bool save)
+        {
+            games.Insert(0, g);
+
+            if (g.WonRun)
+            {
+                ++victories;
+            }
+            else
+            {
+                ++defeats;
+            }
+
+            while (games.Count > numberofgames)
+            {
+                if (games[numberofgames].WonRun)
+                {
+                    --victories;
+                }
+                else
+                {
+                    --defeats;
+                }
+
+                games.RemoveAt(numberofgames);
+            }
+
+            if (save)
+            {
+                CalculateStats();
+                UpdateLabels();
+                SaveGames();
+            }
+        }
+
+        private void UpdateLabels ()
+        {
+            lbl_WinCount.Text = victories.ToString();
+            lbl_LossCount.Text = defeats.ToString();
+        }
+
+        private void LoadGames()
+        {
+            while (games.Count > 0)
+            {
+                games.RemoveAt(0);
+            }
+
+            try
+            {
+                using (System.IO.StreamReader file = new System.IO.StreamReader(@".\games.txt"))
+                {
+                    String line;
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        string characterString;
+                        int characterNumber;
+                        Boolean b;
+
+                        characterString = line.Split(';')[0];
+                        Int32.TryParse(characterString, out characterNumber);
+                        b = line.Split(';')[1] == "1";
+
+                        if (characterNumber < 0 || characterNumber > characters.Length)
+                        {
+
+                        }
+                        else
+                        {
+                            Game game = new IGGnis_Game_Tracker.Form1.Game(characters[characterNumber], b);
+                            AddGame(game, false);
+                        }
+                    }
+                }
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                // No file was found.  Create an empty file.
+                System.IO.StreamWriter fileWrite = new System.IO.StreamWriter(@".\games.txt");
+                fileWrite.Flush();
+                fileWrite.Close();
+            }
+        }
+
+        private void SaveGames ()
+        {
+            try
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@".\games.txt", false))
+                {
+                    var chars = characters.ToArray<String>();
+
+                    for (int i = games.Count - 1; i >= 0; -- i)
+                    {
+                        file.WriteLine(String.Format("{0};{1}", Array.IndexOf(chars, games[i].Character), (games[i].WonRun == true ? "1" : "0")));
+                    }
+
+                    file.Flush();
+                    file.Close();
+                }
+
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@".\wl.txt", false))
+                {
+                    file.WriteLine(String.Format("{0}-{1}", victories, defeats));
+                    file.Flush();
+                    file.Close();
+                }
+
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@".\longest.txt", false))
+                {
+                    file.WriteLine(String.Format("{0}", longeststreaks[characters.Length]));
+                    file.Flush();
+                    file.Close();
+                }
+
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@".\current.txt", false))
+                {
+                    file.WriteLine(String.Format("{0}", streaks[characters.Length]));
+                    file.Flush();
+                    file.Close();
+                }
+
+                for (int c = 0; c < characters.Length; ++c)
+                {
+                    string whichCharacter = characters[c].ToLower();
+                    if (whichCharacter.Equals("???"))
+                    {
+                        whichCharacter = "blue baby";
+                    }
+
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(String.Format(@".\{0}_current.txt", whichCharacter)))
+                    {
+                        file.WriteLine(String.Format("{0}", streaks[c]));
+                        file.Flush();
+                        file.Close();
+                    }
+
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(String.Format(@".\{0}_longest.txt", whichCharacter)))
+                    {
+                        file.WriteLine(String.Format("{0}", longeststreaks[c]));
+                        file.Flush();
+                        file.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            while (games.Count > 0)
+            {
+                games.RemoveAt(0);
+            }
+
+            victories = 0;
+            defeats = 0;
+            longeststreak = 0;
+            currentstreak = 0;
+
+            SaveGames();
+            UpdateLabels();
+            CalculateStats();
+        }
+    }
+}
